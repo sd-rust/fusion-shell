374737b2512a275cf995d31c7c71bb48eb0ba553
Sandeep Datta <datta.sandeep@gmail.com>
2016-07-05 23:10:34 +0530

    Added REPL.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
 #![feature(plugin)]
 #![plugin(peg_syntax_ext)]
 
+use std::io;
+use std::io::prelude::*;
 use arithmetic::expression;
 
 peg_file! arithmetic("fsh.peg");
 
+fn do_repl() {
+    let prompt = "expression > ";
+    print!("{}", prompt);
+    io::stdout().flush().unwrap();
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock();
+    let mut buff = String::new();
+    loop {
+        let _ = stdin.read_line(&mut buff).unwrap();
+        {
+            let line = buff.trim_right_matches("\n");
+
+            if line == "exit" {
+                break;
+            }
+
+            println!("{:?}", expression(&line));
+            print!("{}", prompt);
+            io::stdout().flush().unwrap();
+        }
+        buff.clear();
+    }
+}
+
 fn main() {
-    assert_eq!(expression("1+1"), Ok(2));
-    assert_eq!(expression("5*5"), Ok(25));
-    assert_eq!(expression("222+3333"), Ok(3555));
-    assert_eq!(expression("2+3*4"), Ok(14));
-    assert_eq!(expression("(2+2)*3"), Ok(12));
-    assert!(expression("(22+)+1").is_err());
-    assert!(expression("1++1").is_err());
-    assert!(expression("3)+1").is_err());
+    do_repl();
 }
