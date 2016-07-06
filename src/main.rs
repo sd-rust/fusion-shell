db9cfe7134415d9738ae599d93aab40018f9c729
Sandeep Datta <datta.sandeep@gmail.com>
2016-07-06 23:27:21 +0530

    +cd +pwd

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@
 use std::io;
 use std::io::prelude::*;
 use std::process;
+use std::env;
+use std::path::Path;
 use fsh_parser::program;
 
 #[derive(Clone, PartialEq, Eq, Debug)]
@@ -28,11 +30,57 @@ static BANNER:&'static str = r#"
 |_|   \__ _|___/_|\___/|_| |_| |_____/|_| |_|\___|_|_|
 "#;
 
+//MFU commands: history | cut -d" " -f1 | sort | uniq -c | sort -r -n -k 1 | head -50
+
+fn pwd(args: Vec<Expression>) {
+    match &args[..] {
+        &[] => {
+            let p = env::current_dir().unwrap();
+            println!("{}", p.display());
+        },
+        _ => println!("Ignoring malformed command.")
+    }
+}
+
+fn cd(args: Vec<Expression>) {
+    match &args[..] {
+        &[Expression::Str(ref s)] => {
+            let root = Path::new(s);
+            if env::set_current_dir(&root).is_ok() {
+                println!("Successfully changed working directory to {}!", root.display());
+            } else {
+                println!("Error: could not set current directory.");
+            }
+        },
+        &[] => {
+            let p = env::current_dir().unwrap();
+            println!("{}", p.display());
+        },
+        _ => println!("Ignoring malformed command.")
+    }
+}
+
+// fn grep(_args: Vec<Expression>) {
+//     println!("Not yet implemented!");
+// }
+
+// fn find(_args: Vec<Expression>) {
+//     println!("Not yet implemented!");
+// }
+
+// fn ls(_args: Vec<Expression>) {
+//     println!("Not yet implemented!");
+// }
+
+// fn echo(_args: Vec<Expression>) {
+//     println!("Not yet implemented!");
+// }
+
 fn exit(args: Vec<Expression>) {
     match &args[..] {
         &[Expression::Int(exit_code)] => process::exit(exit_code as i32),
         &[] => process::exit(0),
-        _ => println!("Ignoring malformed exit command.")
+        _ => println!("Ignoring malformed command.")
     }
 }
 
@@ -43,7 +91,9 @@ fn run_prog(prog: Vec<Expression>) {
             Expression::Str(s) => println!("{}", s),
             Expression::CommandApp(name, args) => {
                 match name.as_str() {
-                    "exit" => exit(args),
+                    "pwd"   => pwd(args),
+                    "cd"    => cd(args),
+                    "exit"  => exit(args),
                     _ => println!("Ignoring unknown command: {}", name)
                 }
             }
