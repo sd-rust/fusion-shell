6a713b19fedefecbb1152e2d540844e44319d230
Sandeep Datta <datta.sandeep@gmail.com>
2016-07-06 13:28:05 +0530

    Banner added.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,16 @@ use arithmetic::expression;
 
 peg_file! arithmetic("fsh.peg");
 
+//Font: Big. http://patorjk.com/software/taag/#p=display&f=Big&t=Fusion
+static BANNER:&'static str = r#"
+ ______         _                _____ _          _ _ 
+|  ____|       (_)              / ____| |        | | |
+| |__ _   _ ___ _  ___  _ __   | (___ | |__   ___| | |
+|  __| | | / __| |/ _ \|  _ \   \___ \|  _ \ / _ \ | |
+| |  | |_| \__ \ | (_) | | | |  ____) | | | |  __/ | |
+|_|   \__ _|___/_|\___/|_| |_| |_____/|_| |_|\___|_|_|
+"#;
+
 fn do_repl() {
     let prompt = "expression > ";
     print!("{}", prompt);
@@ -32,5 +42,6 @@ fn do_repl() {
 }
 
 fn main() {
+    println!("{}", BANNER);
     do_repl();
 }
