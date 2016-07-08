9458e47e7be3453df634bd70b3acdb60f43698ce
Sandeep Datta <datta.sandeep@gmail.com>
2016-07-08 22:06:22 +0530

    print_err_ln!() moved to utils.rs

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,8 @@ use std::env;
 use std::path::Path;
 use fsh_parser::program;
 
-macro_rules! print_err_ln {
-    ($($arg:tt)*) => ({
-        use std::io::Write;
-        match writeln!(&mut ::std::io::stderr(), $($arg)* ) {
-            Ok(_) => {},
-            Err(x) => panic!("Unable to write to stderr (file handle closed?): {}", x),
-        }
-    })
-}
+#[macro_use]
+mod utils;
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum Expression {
---
+++ src/utils.rs
@@ -0,0 +1,9 @@
+macro_rules! print_err_ln {
+    ($($arg:tt)*) => ({
+        use std::io::Write;
+        match writeln!(&mut ::std::io::stderr(), $($arg)* ) {
+            Ok(_) => {},
+            Err(x) => panic!("Unable to write to stderr (file handle closed?): {}", x),
+        }
+    })
+}
